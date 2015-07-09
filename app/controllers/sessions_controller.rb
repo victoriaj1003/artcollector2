b08dff105d38805be3d11d468b4192c026029871
victoriaj13@gmail.com <victoriaj13@gmail.com>
2015-07-09 15:26:35 -0400

    added user functionality, stories belong to users, login/signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,18 @@
 class SessionsController < ApplicationController
 
 	def new
+
 	end
 
 	def create
 		@user = User.find_by_email(params[:email])
+		if @user and @user.password == params[:password]
 		session[:user_id] = @user.id
-		redirect_to username_path(@user.username), notice: "successfully logged in"
+		redirect_to dashboard_path(@user.username), notice: "successfully logged in"
 	else
 		flash[:alert] = "there was a problem, please try again"
 		render :new
+	end
 	end
 
 
